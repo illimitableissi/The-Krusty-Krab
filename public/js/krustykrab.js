b3ddb608987cca5ee558ed68c60c0faa50712800
Colin Green <colin.green@knights.ucf.edu>
2019-12-07 11:00:32 -0500

    Figuring out how to connect buttons to input fields

--- public/js/krustykrab.js
+++ public/js/krustykrab.js
@@ -1,7 +1,14 @@
 $(".btn-success").on("click", function(event) {
-    event.preventdefault();
+    event.preventDefault();
 
-    var userchoice = $(this).attr("data topic")
+    var userchoice = $(this).attr("data-topic");
+    
+    var price = parseFloat($(`#${userchoice}`).attr("data-price"));
+    var quantity = parseFloat($(`#${userchoice}`).val());
+    var totalPrice = price * quantity;
 
+    console.log(price);
+    console.log(quantity);
+    console.log(totalPrice);
 
 });
