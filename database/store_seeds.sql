476d9c37f3d0ec83290125b389dc25fda9eb0d59
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-07 12:16:22 -0500

    Added sample size data into Menu_Items table for DB Testing

---
+++ database/store_seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO Item_Type (item_type_name) VALUES ('Entree'); 
+
+INSERT INTO Item_Type (item_type_name) VALUES ('Side'); 
+
+INSERT INTO Item_Type (item_type_name) VALUES ('Drink'); 
+
+-- ----------------------------------------------------------
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 3.25, 850); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Salty Sea Dog', 'Entree', 3.25, 500); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Footlong', 'Entree', 2.00, 400); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krusty Krab Pizza', 'Entree', 5.00, 850);
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('', 'Entree', 3.25, 850); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 3.25, 850); 
+
+
+
