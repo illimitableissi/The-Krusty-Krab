8d392e887b10742339b6a8cb9668fcde272c6494
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-07 11:47:14 -0500

    Created All Parent and Child SQL Tables in krusty_krab DB

--- database/store_schema.sql
+++ database/store_schema.sql
@@ -6,10 +6,32 @@ USE krusty_krab;
 
 CREATE TABLE Item_Type(
     item_type_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-    item_type VARCHAR(255) NOT NULL,
-)
+    item_type VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE menu_items(
+CREATE TABLE Menu_Items(
     item_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
     item_name VARCHAR(255) NOT NULL, 
-)
+    item_type_id INT NOT NULL, 
+    item_price FLOAT NOT NULL, 
+    item_calories INT NOT NULL, 
+    UNIQUE (item_name),
+    FOREIGN KEY (item_type_id) REFERENCES Item_Type(item_type_id)
+);
+
+CREATE TABLE Items_Ordered(
+st_item_ordered INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+item_name VARCHAR(255) NOT NULL, 
+order_id INT NOT NULL, 
+quantity INT NOT NULL, 
+item_price FLOAT NOT NULL, 
+total_price_item FLOAT NOT NULL, 
+FOREIGN KEY (item_name) REFERENCES Menu_Items(item_name)
+);
+
+CREATE TABLE Orders(
+    customer_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    order_date DATETIME NOT NULL, 
+    order_time DATETIME NOT NULL, 
+    order_total FLOAT NOT NULL
+);
