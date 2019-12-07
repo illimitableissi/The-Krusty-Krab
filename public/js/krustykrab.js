00db3a4dfa8cd8aef4574f9a797862f4cf4fade1
SOVEREIGN <isimemeni@gmail.com>
2019-12-07 11:37:01 -0500

    added data-name to html

--- public/js/krustykrab.js
+++ public/js/krustykrab.js
@@ -2,7 +2,7 @@ $(".btn-success").on("click", function(event) {
     event.preventDefault();
 
     var userchoice = $(this).attr("data-topic");
-    
+    var itemName = $(this).attr("data-name");   
     var price = parseFloat($(`#${userchoice}`).attr("data-price"));
     var quantity = parseFloat($(`#${userchoice}`).val());
     var totalPrice = price * quantity;
@@ -10,5 +10,17 @@ $(".btn-success").on("click", function(event) {
     console.log(price);
     console.log(quantity);
     console.log(totalPrice);
+
+    $(".orderlist").append(`${itemName} = ${totalPrice} <br>` )
+    $(".modal").toggle("modal");
+
+    $("#close-button").on("click", function(event) {
+        event.preventDefault();
+        $(".modal").hide("modal");
+        });
+        $(".close-icon").on("click", function(event) {
+        event.preventDefault();
+        $(".modal").hide("modal");
+        });
 
 });
