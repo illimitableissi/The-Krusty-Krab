650d035dc125509ee04c6376661dc3d9af9f9d08
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-07 13:04:57 -0500

    Added another menu item type to Item_Type table and populated entrees

--- database/store_seeds.sql
+++ database/store_seeds.sql
@@ -2,21 +2,33 @@ INSERT INTO Item_Type (item_type_name) VALUES ('Entree');
 
 INSERT INTO Item_Type (item_type_name) VALUES ('Side'); 
 
-INSERT INTO Item_Type (item_type_name) VALUES ('Drink'); 
+INSERT INTO Item_Type (item_type_name) VALUES ('Drink');
+
+INSERT INTO Item_Type (item_type_name) VALUES ('Dessert'); 
 
 -- ----------------------------------------------------------
+--                         Entrees
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 1.25, 00); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 3.25, 850); 
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Double Krabby Patty', 'Entree', 2.00, 850); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Salty Sea Dog', 'Entree', 3.25, 500); 
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Triple Krabby Patty', 'Entree', 3.00, 850); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Footlong', 'Entree', 2.00, 400); 
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Krabby Meal', 'Entree', 3.50, 850); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krusty Krab Pizza', 'Entree', 5.00, 850);
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Double Krabby Meal', 'Entree', 3.75, 950); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('', 'Entree', 3.25, 850); 
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Triple Krabby Meal', 'Entree', 4.00, 1050); 
 
-INSERT INTO Menu_Items (item_name, item_type_name, order_id, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 3.25, 850); 
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Salty Sea Dog', 'Entree', 1.25, 450); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Footlong', 'Entree', 2.00 400); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Krusty Krab Pizza', 'Entree', 5.00, 850);
+
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('', 'Entree', 3.25, 850); 
+
+INSERT INTO Menu_Items (item_name, item_type_name, item_price, item_calories) VALUES ('Krabby Patty', 'Entree', 3.25, 850); 
 
 
 
