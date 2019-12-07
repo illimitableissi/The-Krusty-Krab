7fdbfc452179027ceb0582c4963a20769e561b7b
SOVEREIGN <isimemeni@gmail.com>
2019-12-07 12:04:21 -0500

    added modal information

--- public/js/krustykrab.js
+++ public/js/krustykrab.js
@@ -1,23 +1,24 @@
+var totalOrderPrice = 0
+
 $(".btn-success").on("click", function(event) {
     event.preventDefault();
 
     var userchoice = $(this).attr("data-topic");
     var itemName = $(this).attr("data-name");   
-    var price = parseFloat($(`#${userchoice}`).attr("data-price"));
-    var quantity = parseFloat($(`#${userchoice}`).val());
+    var price = parseFloat($(`#${userchoice}`).attr("data-price"))
+    var quantity = parseInt($(`#${userchoice}`).val());
     var totalPrice = price * quantity;
+    var total = parseFloat(totalPrice).toFixed(2)
+    totalOrderPrice += totalPrice;
 
     console.log(price);
     console.log(quantity);
     console.log(totalPrice);
 
-    $(".orderlist").append(`${itemName} = ${totalPrice} <br>` )
+    $(".orderlist").append(`${itemName} = $${total} <br>`)
+    $(".ordertotal").text(`Order Total = $${totalOrderPrice.toFixed(2)}`)
     $(".modal").toggle("modal");
 
-    $("#close-button").on("click", function(event) {
-        event.preventDefault();
-        $(".modal").hide("modal");
-        });
         $(".close-icon").on("click", function(event) {
         event.preventDefault();
         $(".modal").hide("modal");
