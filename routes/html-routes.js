0565403ca3fbb6adb5da2914fdd6c22fa49451fd
farah asadulla <fasadulla@gmail.com>
2019-12-10 19:49:33 -0500

    made changes to the routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,12 +10,16 @@ module.exports = function(app) {
         res.SendFile(path.join(__dirname, "../public/krustykrab.html"));
     })
 
+    app.get("/receipt", function(req, res) {
+        res.SendFile(path.join(__dirname, "../public/receipt.html"));
+    })
+
     app.get("/orders", function(req, res) {
         res.SendFile(path.join(__dirname, "../public/orders.html"));
     })
 
     app.get("*", function(req, res) {
-        res.SendFile(path.join(__dirname, "../public/404.html"));
+        res.SendFile(path.join(__dirname, "../public/index.html"));
     })
 
 }
--- server.js
+++ server.js
@@ -6,15 +6,19 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+// var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// db.sequelize.sync({ force: true }).then(function() {
+
+// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
