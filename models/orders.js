edb3029585dfbd47d0c6aea143d620cf1f65a2a8
Colin Green <colin.green@knights.ucf.edu>
2019-12-10 20:07:25 -0500

    Finished updating the sequelize models

---
+++ models/orders.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+    var Order = sequelize.define("Order", {
+      customer_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      order_total: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+      }
+    });
+    return Order;
+  };
