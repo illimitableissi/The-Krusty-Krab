24e2135c8f952894d24836f3bcf5463deea77936
Colin Green <colin.green@knights.ucf.edu>
2019-12-11 15:21:42 -0500

    Added a 404 html page and route

--- js_routes/html-routes.js
+++ js_routes/html-routes.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
     })
 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/404.html"));
     })
 
 }
