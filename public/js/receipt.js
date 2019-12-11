a6b2b6bbf4cc2fe9c4067de6c9a60f578f4c9458
Colin Green <colin.green@knights.ucf.edu>
2019-12-11 15:13:13 -0500

    Made minor style changes to the receipt page

--- public/js/receipt.js
+++ public/js/receipt.js
@@ -7,15 +7,27 @@ function runReceiptQuery() {
 
         console.log(receiptData);
         var orderTotal = 0;
+        var pieCheck = false;
 
         for (let i = 0; i < receiptData.length; i++) {
 
-            $(".order-details").append(`${receiptData[i].item_quantity}x ${receiptData[i].item_name} @ $${parseFloat(receiptData[i].item_price).toFixed(2)} = $${receiptData[i].total}<br>`);
+            var itemString = `${receiptData[i].item_quantity}x ${receiptData[i].item_name} @ $${parseFloat(receiptData[i].item_price).toFixed(2)} = $${receiptData[i].total}<br>`;
+            $(".order-details").append(itemString);
             orderTotal += parseFloat(receiptData[i].total);
+            
+            if (itemString.includes("Pie Bomb")) {
+                pieCheck = true;
+            }
             
         }
 
-        $(".order-details").append(`<hr>Total: $${orderTotal.toFixed(2)}`);
+        console.log(`Pie Check: ${pieCheck}`);
+
+        if (orderTotal == 0 && !pieCheck) {
+            $(".order-details").text("You haven't ordered yet! Click the button below to get started.");
+        } else {
+            $(".order-details").append(`<hr>Total: $${orderTotal.toFixed(2)}`);
+        }
 
         clearReceipt();
 
