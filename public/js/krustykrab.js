0cd7ce64cb94a05e61e29e33e07751d9f004e15f
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-12 20:34:21 -0500

    Making sure order quant is not null

--- public/js/krustykrab.js
+++ public/js/krustykrab.js
@@ -8,14 +8,16 @@ var quantity;
 var totalPrice;
 var total;
 
-var newOrder = {orderList: []};
+var newOrder = { orderList: [] };
 var newItem = {};
 
-$(".btn-success").on("click", function(event) {
+$(".btn-success").on("click", function (event) {
+
+
     event.preventDefault();
     $("#receipt").hide();
     userchoice = $(this).attr("data-topic");
-    itemName = $(this).attr("data-name");   
+    itemName = $(this).attr("data-name");
     price = parseFloat($(`#${userchoice}`).attr("data-price"))
     quantity = parseInt($(`#${userchoice}`).val());
     totalPrice = price * quantity;
@@ -23,24 +25,27 @@ $(".btn-success").on("click", function(event) {
     totalOrderPrice += totalPrice;
 
     $(`#${userchoice}`).val('');
+    if (quantity) {
+        newItem = {
+            item_name: itemName,
+            item_price: price,
+            item_quantity: quantity,
+            total: total
+        };
 
-    newItem = {
-        item_name: itemName,
-        item_price: price,
-        item_quantity: quantity,
-        total: total
+        newOrder.orderList.push(newItem);
+        console.log(newOrder);
+
+        $(".orderlist").append(`${itemName} $${price} x ${quantity} = $${total} <br>`);
+        $(".ordertotal").text(`Order Total = $${totalOrderPrice.toFixed(2)}`);
+        $(".modal").toggle("modal");
+    } else {
+        alert('You must enter a quantity greater than 0.');
     };
 
-    newOrder.orderList.push(newItem);
-    console.log(newOrder);
-
-    $(".orderlist").append(`${itemName} $${price} x ${quantity} = $${total} <br>`);
-    $(".ordertotal").text(`Order Total = $${totalOrderPrice.toFixed(2)}`);
-    $(".modal").toggle("modal");
-    
 });
 
-$("#view-order").click(function(event) {
+$("#view-order").click(function (event) {
     event.preventDefault();
     $(".modal").show("modal");
     $("#receipt").hide();
@@ -48,31 +53,30 @@ $("#view-order").click(function(event) {
     $(".close-icon").show();
 })
 
-$(".close-icon").on("click", function(event) {
+$(".close-icon").on("click", function (event) {
     event.preventDefault();
     $(".modal").hide("modal");
 });
 
-$(".checkout").on("click", function(event)
-    {
-        event.preventDefault();
-        $(".modal").show("modal");
-        $("#receipt").show();
-        $(".checkout").hide();
-        $(".close-icon").hide();
-        $(".modal-body").html("<h1>" + "Enjoy your food!" + "</h1>");
+$(".checkout").on("click", function (event) {
+    event.preventDefault();
+    $(".modal").show("modal");
+    $("#receipt").show();
+    $(".checkout").hide();
+    $(".close-icon").hide();
+    $(".modal-body").html("<h1>" + "Enjoy your food!" + "</h1>");
 
-        var orderTotal = {order_total: totalOrderPrice}
+    var orderTotal = { order_total: totalOrderPrice }
 
-        $.post("/api/receipt", newOrder, function(data) {
-            console.log("Receipt Data:");
-            console.log(data);
-        })
+    $.post("/api/receipt", newOrder, function (data) {
+        console.log("Receipt Data:");
+        console.log(data);
+    })
 
-        $.post("/api/orders", orderTotal, function(data) {
-            console.log("Order data:");
-            console.log(data);
-        })
+    $.post("/api/orders", orderTotal, function (data) {
+        console.log("Order data:");
+        console.log(data);
+    })
 
         for (let i = 0; i < newOrder.orderList.length; i++) {
 
